dff0c4a5525875b6098389357f565d5a09bc39b1
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-04-13 20:29:04 +0900

    Add eslint-plugin-import, eslint-plugin-react-hooks and eslint-plugin-jsx-a11y to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -18,12 +22,19 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'import'],
   rules: {
+    'sort-imports': 0,
+    'import/order': [2, { alphabetize: { order: 'asc' } }],
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
   },
   settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts'],
+      },
+    },
     react: {
       version: 'detect',
     },
