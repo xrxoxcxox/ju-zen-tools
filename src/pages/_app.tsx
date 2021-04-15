d0751f709f874c895e1a5989ed282ad69c3537c1
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-04-15 20:11:47 +0900

    Add style to Timer page and Index page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { FC } from 'react'
 import { customProperty } from '../styles/cssCustomProperty'
 import { global } from '../styles/global'
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700"
+        rel="stylesheet"
+      />
+    </Head>
     <Global styles={[customProperty, global]} />
     <Component {...pageProps} />
   </>
--- src/pages/timer/index.tsx
+++ src/pages/timer/index.tsx
@@ -1,5 +1,9 @@
 import { css } from '@emotion/react'
+import Head from 'next/head'
 import { FC, useState, useEffect } from 'react'
+import { Button } from '../../components/Button'
+import { aliasColor } from '../../styles/color'
+import { typography } from '../../styles/typography'
 
 const TimerPage: FC = () => {
   // Get a value from each input element.
@@ -8,13 +12,15 @@ const TimerPage: FC = () => {
   const [second, setSecond] = useState<number>(0)
 
   // Convert the retrieved values to seconds and sum them for calculation.
+  const timeAtInput = hour * 3600 + minute * 60 + second
   const [totalSecond, setTotalSecond] = useState<number>(0)
   useEffect(() => {
-    setTotalSecond(hour * 3600 + minute * 60 + second)
-  }, [hour, minute, second])
+    setTotalSecond(timeAtInput)
+  }, [timeAtInput])
 
   // Set the start or stop state.
   const [measurement, setMeasurement] = useState<boolean>(false)
+  const [editable, setEditable] = useState<boolean>(true)
 
   // Minus 1 second from the current timer value.
   const countDown = () => {
@@ -36,57 +42,196 @@ const TimerPage: FC = () => {
 
   // Reset the timer.
   const reset = () => {
-    setHour(0)
-    setMinute(0)
-    setSecond(0)
+    setTotalSecond(timeAtInput)
+    setMeasurement(false)
   }
 
   return (
-    <div css={style}>
-      <h1>タイマーのページです</h1>
-      <input
-        type="number"
-        min={0}
-        max={99}
-        css={style}
-        onChange={(e): void => setHour(Number(e.target.value))}
-        value={hour}
-      />
-      <input
-        type="number"
-        min={0}
-        max={59}
-        css={style}
-        onChange={(e): void => setMinute(Number(e.target.value))}
-        value={minute}
-      />
-      <input
-        type="number"
-        min={0}
-        max={59}
-        css={style}
-        onChange={(e): void => setSecond(Number(e.target.value))}
-        value={second}
-      />
-      <button css={style} onClick={() => setMeasurement(true)}>
-        スタート
-      </button>
-      <button css={style} onClick={() => setMeasurement(false)}>
-        一時停止
-      </button>
-      <button css={style} onClick={reset}>
-        リセット
-      </button>
-      <p>
-        {displayHour}時間{displayMinute}分{displaySecond}秒
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Timer</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div css={layoutStyle}>
+        {editable ? (
+          <>
+            <div css={inputTimeStyle}>
+              <div css={timerItemStyle}>
+                <input
+                  type="number"
+                  min={0}
+                  max={99}
+                  onChange={(e): void => setHour(Number(e.target.value))}
+                  value={hour}
+                  css={inputStyle}
+                />
+                <span css={unitStyle}>時間</span>
+              </div>
+              <div css={timerItemStyle}>
+                <input
+                  type="number"
+                  min={0}
+                  max={59}
+                  onChange={(e): void => setMinute(Number(e.target.value))}
+                  value={minute}
+                  css={inputStyle}
+                />
+                <span css={unitStyle}>分</span>
+              </div>
+              <div css={timerItemStyle}>
+                <input
+                  type="number"
+                  min={0}
+                  max={59}
+                  onChange={(e): void => setSecond(Number(e.target.value))}
+                  value={second}
+                  css={inputStyle}
+                />
+                <span css={unitStyle}>秒</span>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div css={displayTimeStyle(measurement)}>
+              <div css={timerItemStyle}>
+                <span css={displayNumberStyle}>{displayHour}</span>
+                <span css={unitStyle}>時間</span>
+              </div>
+              <div css={timerItemStyle}>
+                <span css={displayNumberStyle}>{displayMinute}</span>
+                <span css={unitStyle}>分</span>
+              </div>
+              <div css={timerItemStyle}>
+                <span css={displayNumberStyle}>{displaySecond}</span>
+                <span css={unitStyle}>秒</span>
+              </div>
+            </div>
+          </>
+        )}
+        <div css={buttonWrapStyle}>
+          {measurement ? (
+            <>
+              <Button
+                onClick={() => setMeasurement(false)}
+                variant="border"
+                color="main"
+                css={buttonStyle}
+              >
+                一時停止
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={() => {
+                  setMeasurement(true)
+                  setEditable(false)
+                }}
+                variant="fill"
+                color="main"
+                css={buttonStyle}
+              >
+                {editable ? 'スタート' : '再開'}
+              </Button>
+              {editable || (
+                <Button
+                  onClick={() => {
+                    reset()
+                    setEditable(true)
+                  }}
+                  variant="fill"
+                  color="sub"
+                  css={[buttonStyle, resetButtonStyle]}
+                >
+                  リセット
+                </Button>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </>
   )
 }
 
-const style = css({
-  fontSize: 50,
-  lineHeight: 1.5,
+const layoutStyle = css({
+  display: 'grid',
+  gridTemplateRows: '1fr 1fr',
+  height: '100%',
+  placeItems: 'center',
+})
+
+const inputTimeStyle = css({
+  alignSelf: 'flex-end',
+  backgroundColor: aliasColor.surface,
+  borderRadius: 20,
+  display: 'flex',
+  lineHeight: 1,
+  padding: '24px 48px 28px',
+})
+
+const displayTimeStyle = (measurement: boolean) =>
+  css({
+    alignSelf: 'flex-end',
+    display: 'flex',
+    lineHeight: 1,
+    padding: '24px 48px 28px',
+    color: measurement ? 'inherit' : aliasColor.textDisabled,
+  })
+
+const timerItemStyle = css({
+  display: 'flex',
+  alignItems: 'baseline',
+  '& + &': {
+    marginLeft: 32,
+  },
+})
+
+const inputStyle = css({
+  backgroundColor: 'transparent',
+  border: 'none',
+  color: aliasColor.textOnSurface,
+  fontFamily: '"Noto Serif", serif',
+  fontSize: 96,
+  height: '1em',
+  textAlign: 'center',
+  width: 108,
+  MozAppearance: 'textfield',
+  '&::-webkit-inner-spin-button, &::-webkit-outer-spin-button': {
+    WebkitAppearance: 'none',
+  },
+})
+
+const displayNumberStyle = css({
+  fontFamily: '"Noto Serif", serif',
+  fontSize: 96,
+  textAlign: 'center',
+  width: 108,
+})
+
+const unitStyle = css(typography.subhead2, {
+  marginLeft: 8,
+})
+
+const buttonWrapStyle = css({
+  alignItems: 'center',
+  alignSelf: 'flex-start',
+  display: 'flex',
+  flexDirection: 'column',
+  paddingTop: 64,
+  width: '100%',
+})
+
+const buttonStyle = css({
+  width: 'min(400px, 100%)',
+})
+
+const resetButtonStyle = css({
+  marginTop: 16,
 })
 
 export default TimerPage
