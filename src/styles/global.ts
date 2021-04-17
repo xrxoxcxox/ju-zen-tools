4c2196dd1d10655cc7eae93baf60a4c99409fc57
Keisuke Watanuki <watanuki.keisuke@a-tm.co.jp>
2021-04-18 08:08:45 +0900

    Change global outline style for accessibility

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -23,7 +23,7 @@ export const global = css({
   '#__next': {
     display: 'contents',
   },
-  '[data-whatintent="mouse"] *:focus, [data-whatintent="touch"] *:focus': {
+  '[data-whatinput="mouse"] *:focus, [data-whatinput="touch"] *:focus': {
     outline: 'none',
   },
   'h1, h2, h3, h4, h5, h6': {
