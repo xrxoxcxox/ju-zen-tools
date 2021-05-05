cba442cff1a746c68b22c3060e42310a68c39143
xrxoxcxox <watanuki@ju-zen.com>
2021-05-05 22:46:14 +0900

    Fix type of src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,19 +6,21 @@ import { customProperty } from '../styles/cssCustomProperty'
 import { global } from '../styles/global'
 import 'what-input'
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700"
-        rel="stylesheet"
-      />
-    </Head>
-    <Global styles={[customProperty, global]} />
-    <Component {...pageProps} />
-  </>
-)
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700"
+          rel="stylesheet"
+        />
+      </Head>
+      <Global styles={[customProperty, global]} />
+      <Component {...pageProps} />
+    </>
+  )
+}
 
 export default MyApp
